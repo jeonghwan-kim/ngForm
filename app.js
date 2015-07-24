3f4c68f27c95e5b5cfb53ad26b1165614ead231d
jeonghwan <ej88ej@gmail.com>
2015-07-24 20:52:00 +0900

    Custome validator

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 'use strict';
 
-var app = angular.module('ngFormTest', []);
+angular.module('ngFormTest', []);
 
-app.controller('MainCtrl', function ($scope) {
+angular.module('ngFormTest').controller('MainCtrl', function ($scope) {
 
   $scope.text = 'hello';
 
 });
 
 
+angular.module('ngFormTest').directive('emailValidate', function () {
+  return {
+    require: 'ngModel',
+    restrict: 'A',
+    link: function (scope, element, attrs, modelCtrl) {
+      console.log(modelCtrl);
+
+      modelCtrl.$validators.email = function (modelValue) {
+        var EMAIL_REGEXP = /^[a-zA-Z0-9+@[a-zA-Z0-9]+\.[a-zA-Z]{2,5}$/;
+        return EMAIL_REGEXP.test(modelValue);
+      };
+
+      modelCtrl.$validators.gmail = function (modelValue) {
+        var GMAIL_REGEXP = /gmail\.com/;
+        return GMAIL_REGEXP.test(modelValue);
+      }
+    }
+  };
+});
